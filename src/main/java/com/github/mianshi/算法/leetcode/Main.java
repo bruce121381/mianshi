70c55b1f72403fd0b559943a19dcbcbbe4ee8024
mengxzh <mengxzh@weilaicheng.com>
2020-03-27 16:53:00 +0800

    git init

---
+++ src/main/java/com/github/mianshi/算法/leetcode/Main.java
@@ -0,0 +1,13 @@
+package com.github.mianshi.算法.leetcode;
+
+/**
+ * author by beijita@weilaicheng.com
+ * 2020/3/27
+ *
+ * @author xiangzhi.meng
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/com/github/mianshi/MianshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mianshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MianshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
